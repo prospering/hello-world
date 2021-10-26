f195bf79ce0b7bea90fbeeaceb1729b18f8d08f2
Elodie <elodie.manai@hotmail.fr>
2021-10-26 14:05:13 +0000

    addition of Hello World + wifi connection

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,24 @@
 #include <Arduino.h>
 #include <M5Stack.h>
+#include <WiFi.h>
+
+char* ssid = "Campus2"; 
+const char* pass = "12345678";
 
 void setup() {
   // put your setup code here, to run once:
   M5.begin();
   M5.Power.begin();
 
+  WiFi.begin(ssid, pass);
+
   M5.Lcd.fillScreen(BLACK);
   M5.Lcd.setCursor(0,10);
   M5.Lcd.setTextColor(WHITE);
   M5.Lcd.setTextSize(3);
   M5.Lcd.printf("Hello World");
+  M5.Lcd.printf("\nMy name is ...\n\nProspering");
+  M5.Lcd.printf("\n\nNice meeting you :)");
 
 }
 
