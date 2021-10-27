8f2dfa6069d15e8800c31ab1280712ffe27d191f
Tom Mischler <tom.mischler@le-campus-numerique.fr>
2021-10-27 11:40:13 +0200

    send data to bagtower API via POST requests

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,49 @@
 #include <Arduino.h>
 #include <M5Stack.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ArduinoJson.h>
+StaticJsonDocument<256> doc;
+StaticJsonDocument<256> dataDoc;
 
 void setup() {
   // put your setup code here, to run once:
-  M5.begin();
+  M5.begin(115200);
   M5.Power.begin();
-
-  M5.Lcd.fillScreen(BLACK);
-  M5.Lcd.setCursor(0,10);
-  M5.Lcd.setTextColor(WHITE);
-  M5.Lcd.setTextSize(3);
-  M5.Lcd.printf("Hello World");
-
+  M5.Lcd.textsize = 2;
+  WiFi.begin("CAMPUS NUMERIQUE GRENOBLE", "TQPYE97437");
+  M5.Lcd.println("connecting...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1);
+  }
+  M5.Lcd.clear();
+  M5.Lcd.println("connected");
+  HTTPClient http;
+  http.addHeader("x-api-key", "cHqVZbl1sA248m3bg21uF7AhZqMvySeJ447BDXKe");
+  // http.addHeader("Content-type", "application/json");
+  http.begin("http://api.bagtower.bag-era.fr/prod/logs");
+  JsonObject root = doc.to<JsonObject>();
+  JsonObject data = dataDoc.to<JsonObject>();
+  data["id"] = "title";
+  data["type"] = "string";
+  data["val"] = "testval";
+  root["unixTimestamp"] = 0;
+  root.createNestedArray("data");
+  root["data"].add(data);
+  root["UTCTimestamp"] = "now";
+  root["deviceId"] = "6f34c9b0-1791-1:86539070-365b-11ec-b";
+  String payload;
+  serializeJson(doc, payload);
+  M5.Lcd.println(payload);
+  int httpCode = http.POST(payload);
+  if (httpCode > 0) {
+    String content = http.getString();
+    M5.Lcd.println(httpCode);
+    M5.Lcd.println(content);
+  }
+  else {
+    M5.Lcd.println("error");
+  };
 }
 
 void loop() {
