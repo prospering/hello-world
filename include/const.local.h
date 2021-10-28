36424d1aabd2ccf69b68f57ee92033e4a83f68d3
Elodie <elodie.manai@hotmail.fr>
2021-10-28 09:22:20 +0000

    addition of const.local.h for environment variables + gitignore update

---
+++ include/const.local.h
@@ -0,0 +1,10 @@
+//Device id and api key for BAG Tower
+const char* x_api_key = "CifhSACFQeaS21CNsLMjO3WQ1GjEucyT5ytQXez0";
+const char* deviceId = "6f34c9b0-1791-1:69e1c3d0-365b-11ec-b";
+
+//WiFi connection info
+const char *ssid = "Campus2";
+const char *pass = "12345678";
+
+//ntp server
+const char* ntpServer = "fr.pool.ntp.org";
