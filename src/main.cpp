c0691b950aaf7eefe57a27c40cb53a430961cf81
Tom Mischler <tom.mischler@le-campus-numerique.fr>
2021-10-28 11:36:33 +0200

    merge feature/set-time and fix api calls

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <M5Stack.h>
 #include <WiFi.h>
+#include <time.h>
 #include <HTTPClient.h>
 #include <ArduinoJson.h>
 #include <const.local.h>
@@ -10,7 +11,41 @@ StaticJsonDocument<256> dataDoc;
 void setupM5() {
   M5.begin(115200);
   M5.Power.begin();
-  M5.Lcd.textsize = 2;
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 10);
+  M5.Lcd.setTextColor(WHITE);
+  M5.Lcd.setTextSize(2.5);
+}
+
+void printLocalTime()
+{
+  struct tm timeinfo;
+  if(!getLocalTime(&timeinfo)){
+    M5.Lcd.println("Failed to obtain time");
+    return;
+  }
+  M5.Lcd.println(&timeinfo, "%A, %B %d %Y %H:%M:%S");
+}
+
+//sets up the wifi, sends an error message if the connection is established and prints the ip address if it works
+void setup_wifi()
+{
+  WiFi.begin(ssid, pass);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(1000);
+    M5.Lcd.fillScreen(BLACK);
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.printf("Connecting to WiFi...");
+  }
+
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+
+  M5.Lcd.println("Connected to the WiFi network");
+  M5.Lcd.println(ssid);
+  M5.Lcd.println("\nAdresse IP : ");
+  M5.Lcd.println(WiFi.localIP());
 }
 
 String generateLogJson(String id, String type, String val, int timestamp, String UTCTimestamp, String deviceId) {
@@ -35,19 +70,17 @@ void printMsg(String msg) {
 }
 
 void beginConnect(HTTPClient &http, String url = "https://api.bagtower.bag-era.fr/prod/logs") {
-  http.addHeader("x-api-key", "CifhSACFQeaS21CNsLMjO3WQ1GjEucyT5ytQXez0");
-  http.addHeader("Content-type", "application/json");
   http.begin(url);
+  http.addHeader("Content-type", "application/json");
+  http.addHeader("x-api-key", "cHqVZbl1sA248m3bg21uF7AhZqMvySeJ447BDXKe");
 }
 
 void setup() {
   setupM5();
-  WiFi.begin(ssid, pass);
-  printMsg("connecting...");
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1);
-  }
-  printMsg("connected");
+  setup_wifi();
+  //init and get the time
+  configTime(3600, 3600, ntpServer);
+  printLocalTime();
   HTTPClient http;
   beginConnect(http);
   String payload = generateLogJson("title", "string", "value", 0, "now", "6f34c9b0-1791-1:69e1c3d0-365b-11ec-b");
@@ -65,4 +98,14 @@ void setup() {
 }
 
 void loop() {
+  delay(1000);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setCursor(0, 0);
+
+  M5.Lcd.println("Connected to the WiFi network");
+  M5.Lcd.println(ssid);
+  M5.Lcd.println("\nAdresse IP : ");
+  M5.Lcd.println(WiFi.localIP());
+  
+  printLocalTime();
 }
